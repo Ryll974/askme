202863ca6944ed066b86d5304be8dd7e2b057cc1
Ryll974 <jullien.patrick@gmail.com>
2022-01-12 09:28:14 +0400

    commit 2 - ajout fonction BallPage avec utilisation dart:math pour la fonction random - implementation du TextButton à la place de l'image statique

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -9,18 +10,41 @@ void main() {
           centerTitle: true,
           title: Text(
             "Ask me anything !",
-            style: TextStyle(color: Colors.white,fontSize: 50.0, fontWeight: FontWeight.bold,fontFamily: 'GreatVibes',),
+            style: TextStyle(color: Colors.white,fontSize: 50.0,fontFamily: 'GreatVibes',),
           ),
           backgroundColor: Colors.lightBlue,
         ),
-        body: SafeArea(
-          child: Center(
-            child: Image(
-              image: AssetImage("images/ball1.png"),
-            ),
-          ),
-        ),
+        body: BallPage(),
       ),
     ),
   );
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int BallNumber = Random().nextInt(5) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+          child: TextButton(
+            child: Image(
+              image: AssetImage("images/ball$BallNumber.png"),
+            ),
+            onPressed: () {
+              setState ((){
+                BallNumber = Random().nextInt(5) + 1;
+              });
+              print("test boule divination");
+            },
+          )
+          ),
+        );
+  }
 }
