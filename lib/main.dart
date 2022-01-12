3e15c2ee47371111e6bd04d803bc735129442bb8
Ryll974 <jullien.patrick@gmail.com>
2022-01-12 08:47:58 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Ask me anything !",
+            style: TextStyle(color: Colors.white,fontSize: 50.0, fontWeight: FontWeight.bold,fontFamily: 'GreatVibes',),
+          ),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/ball1.png"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
